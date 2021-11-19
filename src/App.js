ae4d4386de50bd312f71746e6629cc8239260ef4
Asiel Leal Celdeiro <lealceldeiro@gmail.com>
2021-11-19 19:38:55 +0200

    Add PoC for login using keycloack

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
 import axios from 'axios';
+import { UserManager } from "oidc-client";
 import './App.css';
 import React from 'react';
 
+class AuthService {
+  static instance = null;
+  userManager = new UserManager({
+    authority: "http://localhost:8888/auth/realms/wcdevs",
+    client_id: "wcdevs-front-client",
+    redirect_uri: "http://localhost:3000/login-callback.html",
+    response_type: "id_token token",
+    scope: "openid profile email"
+  });
+
+  constructor (){
+    this.login = this.login.bind(this);
+    this.getUser = this.getUser.bind(this);
+  }
+
+  static getInstance() {
+    if (this.instance == null) {
+      this.instance = new AuthService();
+    }
+    return this.instance;
+  }
+
+  login() {
+    return this.userManager.signinRedirect()
+  }
+
+  getUser() {
+    return this.userManager.getUser();
+  }
+}
+
 class Form extends React.Component {
-  state = { posts: [] };
   handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await axios.get(`http://localhost:8080/post/`);
+
+    const response = await axios.get(`http://localhost:8080/post/`, {
+      headers: {
+        "Authorization": "Bearer " + sessionStorage.getItem("access_token")
+      }
+    });
 
     this.props.onSubmit(response.data);
   }
 
   render() {
     return (
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={ this.handleSubmit }>
           <button>Fetch posts</button>
         </form>
     );
@@ -22,19 +58,23 @@ class Form extends React.Component {
 
 class App extends React.Component {
   state = { posts: [] };
+  auth = AuthService.getInstance();
 
   setPosts = (posts) => {
     this.setState(previous => ({
       posts: posts
     }))
   }
+
   render() {
     return (
         <div>
-          <Form onSubmit={this.setPosts}/>
+          <Form onSubmit={ this.setPosts }/>
           <div>
-            {JSON.stringify(this.state.posts)}
+            { JSON.stringify(this.state.posts) }
           </div>
+          <hr/>
+          <button onClick={ this.auth.login }>Login</button>
         </div>
     )
   }
