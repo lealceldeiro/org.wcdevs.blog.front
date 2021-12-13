5d5bf1d703ed0c69fc2c6570dccee65ab52a7f1a
Yury <yurydgfuentes@gmail.com>
2021-12-14 00:21:08 +0200

    #7 add mechanism to authenticate with 3rd party libraries

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import NextAuth from "next-auth"
+import CognitoProvider from 'next-auth/providers/cognito';
+
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  // Do whatever you want here, before the request is passed down to `NextAuth`
+  return await NextAuth(req, res, {
+    providers: [
+        CognitoProvider({
+            clientId: process.env.COGNITO_CLIENT_ID || '',
+            issuer: process.env.COGNITO_ISSUER,
+            clientSecret: process.env.COGNITO_CLIENT_SECRET || '',
+          })
+    ],
+    callbacks: {
+      async jwt({ token, account }) {
+        // Persist the OAuth access_token to the token right after signin
+        if (account) {
+          token.accessToken = account.access_token
+        }
+        return token;
+      },
+      async session({ session, token, user }) {
+        // Send properties to the client, like an access_token from a provider.
+        session.accessToken = token.accessToken
+        return session;
+      }
+    },
+    debug: false,
+  })
+}
