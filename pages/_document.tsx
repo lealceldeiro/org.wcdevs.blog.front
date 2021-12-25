3d80333b3bc8a13add80ae7165872c7b725387da
Yury <yurydgfuentes@gmail.com>
2021-12-25 22:08:21 +0200

    #1 add mechanism to use script in all pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-
 class CustomDocument extends Document {
-
     render() {
         return (
             <Html>
@@ -10,7 +8,6 @@ class CustomDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet" />
                     <link href="/static/pavo/css/fontawesome-all.css" rel="stylesheet" />
-                    {/* // <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" /> */}
                     <link href="/static/pavo/css/swiper.css" rel="stylesheet" />
                     <link href="/static/pavo/css/magnific-popup.css" rel="stylesheet" />
                     <link href="/static/pavo/css/styles.css" rel="stylesheet" />
