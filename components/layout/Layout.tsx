752ee8f20e508a5c98b03b397c3aa139b6d5d51f
Yury <yurydgfuentes@gmail.com>
2021-12-28 21:38:50 +0200

    #1 remove header from layout

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -13,8 +13,7 @@ export const Layout: React.FC = ({ children }) => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <Nav />
-                <Header />
+                <Nav />               
                 {children}
                 <Footer />
             </main>
