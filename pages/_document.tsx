64a46021b836bdf4b0780bb9f6594bb98867fd03
Yury <yurydgfuentes@gmail.com>
2021-12-28 21:20:02 +0200

    #1 update custom script for all the pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,18 +8,15 @@ class CustomDocument extends Document {
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet" />
                     <link href="/static/pavo/css/fontawesome-all.css" rel="stylesheet" />
-                    <link href="/static/pavo/css/swiper.css" rel="stylesheet" />
-                    <link href="/static/pavo/css/magnific-popup.css" rel="stylesheet" />
                     <link href="/static/pavo/css/styles.css" rel="stylesheet" />
                 </Head>
                 <body data-spy="scroll" data-target=".fixed-top">
+                    <a href="body" className="back-to-top page-scroll">Back to Top</a>
                     <Main />
                     <NextScript />
                     <script src="/static/pavo/js/jquery.min.js"></script>
                     <script src="/static/pavo/js/jquery.easing.min.js"></script>
-                    <script src="/static/pavo/js/swiper.min.js"></script>
-                    <script src="/static/pavo/js/jquery.magnific-popup.js"></script>
-                    <script src="/static/pavo/js/scripts.js"></script>
+                    <script src="/static/pavo/js/custom.js"></script>
                 </body>
             </Html>
         )
