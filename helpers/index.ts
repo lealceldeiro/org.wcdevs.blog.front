221f58ebe0a5ea3843e73bd7b5c07a594519e575
Yury <yurydgfuentes@gmail.com>
2022-01-07 02:37:32 +0200

    #11 add helper to store info in local storage

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,2 +1,3 @@
 export * from './generateRandomId';
 export * from './createEmptyPost';
+export * from './postStorage';
---
+++ helpers/postStorage.ts
@@ -0,0 +1,18 @@
+import { Post } from "../types";
+
+export const getPost = (postId: string): Post | null => {
+    const foundPost = localStorage.getItem(postId);
+    if (foundPost) {
+        return JSON.parse(foundPost) as Post;
+    }
+    return null
+}
+
+export const savePost = (post: Post) => {
+    const foundPost = localStorage.getItem(post.id);
+    if (foundPost) {
+        localStorage.setItem(post.id, JSON.stringify(post));
+    } else {
+        localStorage.setItem(post.id, JSON.stringify(post));
+    }
+}
