1ed879802e53098c64fc3c607878d29628729504
Yury <yurydgfuentes@gmail.com>
2022-01-17 00:38:42 +0200

    #11 create new hook to handle mocked data

---
+++ hooks/usePost.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { getPost } from "../helpers";
+import { Post } from "../types";
+
+// TODO: remove this after create mock on data Base
+export const usePost = (data: Post) => {
+    const [postLoaded, setPost] = useState<Post | null>(null);
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') { // because windows is not mounted 
+            const postFromStorage = getPost(data.slug);
+            setPost(postFromStorage ? postFromStorage : data);
+        }
+    }, [data]);
+
+    return {
+        postFromStorage: postLoaded
+    }
+}
